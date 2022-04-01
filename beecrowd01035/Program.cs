99ae2b9872f64a3a2bc753fbd09543cf81bfbfa2
Guilherme <gui>
2022-04-01 11:06:52 -0300

    adicionando novos arquivos ao repositorio

---
+++ beecrowd01035/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd01035
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c, d;
+
+
+
+            string l1 = Console.ReadLine();
+            string[] v1 = l1.Split(' ');
+
+            a = int.Parse(v1[0]);
+            b = int.Parse(v1[1]);
+            c = int.Parse(v1[2]);
+            d = int.Parse(v1[3]);
+
+            if (b > c & d > a & c + d > a + b & c > 0 & d > 0 & a % 2 == 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+                Console.WriteLine("Valores nao aceitos");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ beecrowd01036/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd01036
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, b, c, delta, x1, x2;
+
+            string l1 = Console.ReadLine();
+            string[] v1 = l1.Split(' ');
+
+            a = double.Parse(v1[0], System.Globalization.CultureInfo.InvariantCulture);
+            b = double.Parse(v1[1], System.Globalization.CultureInfo.InvariantCulture);
+            c = double.Parse(v1[2], System.Globalization.CultureInfo.InvariantCulture);
+
+            delta = (Math.Pow(b, 2) - 4 * a * c);
+
+            x1 = (-b + (Math.Sqrt(delta))) / (2 * a);
+            x2 = (-b - (Math.Sqrt(delta))) / (2 * a);
+
+            if (double.IsNaN(x1) == true | double.IsNaN(x2) == true)
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+                Console.WriteLine("R1 = " + x1.ToString("F5") + "\nR2 = " + x2.ToString("F5"));
+
+        }
+    }
+}
