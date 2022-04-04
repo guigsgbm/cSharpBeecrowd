7648e07a02263bda819bd0a7044095280cb5f920
Guilherme <gui>
2022-04-04 19:27:50 -0300

    treinando git

---
+++ beecrowd1047/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd1047
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int iJogo, fJogo, iMin, fMin, i = 0, ii=0;
+
+            string l1 = Console.ReadLine();
+            string[] v1 = l1.Split(' ');
+
+            iJogo = int.Parse(v1[0]);
+            iMin = int.Parse(v1[1]);
+            fJogo = int.Parse(v1[2]);
+            fMin = int.Parse(v1[3]);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
