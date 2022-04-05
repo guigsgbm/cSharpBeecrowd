93668201821ac530397249872d9e714c7cfef40a
Guilherme <gui>
2022-04-05 13:57:12 -0300

    Adicionando exercicio 1047 ao repositorio

--- beecrowd1047/Program.cs
+++ beecrowd1047/Program.cs
@@ -10,19 +10,45 @@ namespace beecrowd1047
     {
         static void Main(string[] args)
         {
-
-            int iJogo, fJogo, iMin, fMin, i = 0, ii=0;
+            string iJogo, fJogo;
 
             string l1 = Console.ReadLine();
             string[] v1 = l1.Split(' ');
 
-            iJogo = int.Parse(v1[0]);
-            iMin = int.Parse(v1[1]);
-            fJogo = int.Parse(v1[2]);
-            fMin = int.Parse(v1[3]);
+            iJogo = String.Join(":", v1[0], v1[1]);
+            fJogo = String.Join(":", v1[2], v1[3]);
 
-            
+            var inicioJogo = TimeSpan.Parse(iJogo);
+            var finalJogo = TimeSpan.Parse(fJogo);
+
+            int diferença = TimeSpan.Compare(inicioJogo, finalJogo);
+
+            if (diferença == 0)
+                Console.WriteLine("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
+
+            if (diferença == -1)
+            {
+                TimeSpan intervalo1 = finalJogo - inicioJogo;
+                double totalMinutes = intervalo1.TotalMinutes;
+
+                double i = Math.Floor(totalMinutes / 60);
+                double ii = (totalMinutes % 60);
+                Console.WriteLine("O JOGO DUROU "+ i.ToString("F0") +" HORA(S) E " + ii + " MINUTO(S)");
+            }
+
+            if (diferença == 1)
+            {
+                TimeSpan intervalo1 = finalJogo - inicioJogo;
+                double totalMinutes = intervalo1.TotalMinutes + 1440;
+
+                double i = Math.Floor(totalMinutes / 60);
+                double ii = (totalMinutes % 60);
+                Console.WriteLine("O JOGO DUROU " + i.ToString("F0") + " HORA(S) E " + ii + " MINUTO(S)");
+
+            }
+
             Console.ReadLine();
         }
+
     }
 }
