fba0660eb60a2557a163de57b6eec122a581f143
Guilherme <gui>
2022-04-08 15:47:42 -0300

    adicionando exercicios 1052 e 1059

---
+++ beecrowd1052/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd1052
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+
+            string[] mes = {"","January", "February", "March", "April", "May", "June", "July", "August",
+                "September", "October", "November", "December"};
+
+                Console.WriteLine(mes[a]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ beecrowd1059/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd1059
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
