397ac240fe213e8ca3c7a72df71859bb9788a0b1
Guilherme <gui>
2022-04-27 17:00:18 -0300

    adicionando os exercicios 1070 e 1071

---
+++ beecrowd1070/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd1070
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int[] num = new int[6];
+
+            for (int i = 0; i < num.Length;)
+            {
+                if (x % 2 != 0)
+                {
+                    num[i] = x;
+                    Console.WriteLine(num[i]);
+                    i++;
+                }
+                x++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ beecrowd1071/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd1071
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int r=0;
+
+            if (x < y)
+            {
+                for (int i = x + 1; i != y; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        r += i;
+                    }
+                }
+            }
+
+            if (x > y)
+            {
+                for (int i = x-1; i != y; i--)
+                {
+                    if (i % 2 != 0)
+                    {
+                        r += i;
+                    }
+                }
+            }
+
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+    }
+}
