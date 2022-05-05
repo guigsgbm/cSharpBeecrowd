710b760da8f969adaa8dab52de172dc6bc0563b6
Guilherme <gui>
2022-05-05 15:57:43 -0300

    adicionando os exercicios 1072 e 1073 ao repositorio

---
+++ beecrowd1072/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd1072
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int qin = 0, qout=0;
+            int testCase = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testCase; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                if(x >= 10 && x <= 20)
+                {
+                    qin++;
+                }
+                else
+                    qout++;
+            }
+
+            Console.WriteLine("{0} in\n{1} out", qin, qout);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ beecrowd1073/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd1073
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= x; i+=2)
+            {
+                Console.WriteLine("{0}^2 = {1}", i, i * i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
