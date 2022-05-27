9999a26b47599c86e3596707ff2433f0f8e47265
Guilherme <gui>
2022-05-27 12:36:57 -0300

    Adicionando novos exercicios ao repo

---
+++ beecrowd01022/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd01022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int qtdCases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtdCases; i++)
+            {
+
+                string l1 = Console.ReadLine();
+                string[] v1 = l1.Split(' ');
+
+                int N1 = int.Parse(v1[0]);
+                int D1 = int.Parse(v1[2]);
+                int N2 = int.Parse(v1[4]);
+                int D2 = int.Parse(v1[6]);
+                char op = char.Parse(v1[3]);
+                int R1, R2, S1, S2;
+
+                if (op == '+')
+                {
+                    R1 = (N1 * D2 + N2 * D1);
+                    R2 = (D1 * D2);
+                }
+                else if (op == '-')
+                {
+                    R1 = ((N1 * D2) - (N2 * D1));
+                    R2 = (D1 * D2);
+                }
+                else if (op == '*')
+                {
+                    R1 = (N1 * N2);
+                    R2 = (D1 * D2);
+                }
+                else if (op == '/')
+                {
+                    R1 = (N1 * D2);
+                    R2 = (N2 * D1);
+                }
+                else
+                {
+                    Console.WriteLine("Erro");
+                    return;
+                }
+
+                int mdc = MDC(R1, R2);
+                S1 = R1 / mdc;
+                S2 = R2 / mdc;
+                Console.WriteLine("{0}/{1} = {2}/{3}",R1,R2,S1,S2);
+            }
+            Console.ReadLine();
+        }
+
+        public static int MDC(int a, int b)
+        {
+            int resto;
+
+            while (b != 0)
+            {
+                resto = a % b;
+                a = b;
+                b = resto;
+            }
+
+            if (a < 0)
+                a *= -1;
+            return a;
+        }
+    }
+
+}
---
+++ beecrowd1074/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd1074
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdCases = int.Parse(Console.ReadLine());
+            int [] dados = new int[qtdCases];
+            string [] saida = new string[qtdCases];
+
+            for(int i = 0; i < qtdCases; i++)
+            {
+                dados[i] = int.Parse(Console.ReadLine());
+
+                if (dados[i] % 2 == 0)
+                {
+                    saida[i] = "EVEN ";
+                }
+                else
+                {
+                    saida[i] = "ODD ";
+                }
+
+                if (dados[i] > 0)
+                {
+                    saida[i]+= "POSITIVE";
+                }
+                else
+                {
+                    saida[i] += "NEGATIVE";
+                }
+
+                if (dados[i] == 0)
+                {
+                    saida[i] = "NULL";
+                }
+            }
+
+            foreach (string i in saida)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
